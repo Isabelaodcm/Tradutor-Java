850a3a663ee1b0340ba8006db031ef1987a68904
Isabela <isabela.moreira8721@gmail.com>
2025-10-05 12:29:08 -0300

    Add métodos auxiliares no Parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -7,7 +7,7 @@ public class Parser{
     }
 
     public void parse(){
-        // ainda vai ser implementado
+        expr();
     }
 
     // método que retorna o token(caracter) atual
@@ -16,5 +16,45 @@ public class Parser{
             return(char)input[current];
 
         return '\0';
+    }
+
+    // método que verifica se o atual caracter é igual ao caracter de entrada
+    private void match(char c){
+        if(c == peek()){
+            current++;
+        } else{
+            throw new Error("Syntax error");
+        }
+    }
+
+    void expr() {
+        digit();
+        oper();
+    }
+
+    // Define que são válidos apenas os digitos de 0 a 9
+    void digit () {
+        if (Character.isDigit(peek())) {
+            System.out.println("push " + peek());
+            match(peek());
+        } else {
+            throw new Error("syntax error");
+        }
+    }
+
+
+
+    void oper () {
+        if (peek() == '+') {
+            match('+');
+            digit();
+            System.out.println("add");
+            oper();
+        } else if (peek() == '-') {
+            match('-');
+            digit();
+            System.out.println("sub");
+            oper();
+        }
     }
 }
